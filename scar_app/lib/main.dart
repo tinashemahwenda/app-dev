46d7558b95a947035073c25b39ebf8118be5a475
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-05-25 18:44:05 +0200

    Removed dart:html import

--- scar_app/lib/main.dart
+++ scar_app/lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MainApp());
+void main() => runApp(const MainApp());
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
