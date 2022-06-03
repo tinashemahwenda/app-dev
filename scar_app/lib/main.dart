f51f33b0ef66e211d47e0188103c07120ac59176
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-03 17:06:45 +0200

    Updated main.dart

--- scar_app/lib/main.dart
+++ scar_app/lib/main.dart
@@ -179,7 +179,7 @@ class Register extends StatelessWidget {
                         padding: EdgeInsets.only(top: 3, left: 3),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(40),
-                            border: Border(
+                            border: const Border(
                                 bottom: BorderSide(color: Colors.black),
                                 top: BorderSide(color: Colors.black),
                                 right: BorderSide(color: Colors.black),
