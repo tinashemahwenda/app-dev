e239deeec0a61fbc05a5c71dc7299f4d43ac807d
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-11 06:54:02 +0200

    Update main.dart

--- scar_app/lib/main.dart
+++ scar_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() => runApp(const MainApp());
 
@@ -15,6 +16,23 @@ class MainApp extends StatelessWidget {
         secondaryHeaderColor: Colors.deepOrange,
       ),
       home: const Login(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(children: [
+        Center(
+          child: Image.asset('assets/images/login.png'),
+        ),
+        Text('ScarlabAi')
+      ]),
+      nextScreen: Login(),
     );
   }
 }
